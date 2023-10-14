f677f3454f7857c7b881515e459528a0b15e3ed0
Bongomin Daniel <bongomindaniel@gmail.com>
2023-10-14 23:19:15 +0300

    Main Layout Component

---
+++ frontend/src/components/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import SideBar from './SideBar'
+import Footer from './Footer'
+import NavBar from './NavBar'
+import TopPanel from './TopPanel'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+    return (
+        <>
+            <NavBar />
+            <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
+                <SideBar />
+                <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
+                    <main>
+                        <TopPanel />
+                    </main>
+                    <Outlet />
+                    <Footer />
+                </div>
+            </div>
+        </>
+    )
+}
+export default Layout
