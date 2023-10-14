56a3655d5802269f91637b67f64d2205d24136c9
Bongomin Daniel <bongomindaniel@gmail.com>
2023-10-14 23:41:32 +0300

    set CORS config

--- backend/StudentAndTeachersApp/settings.py
+++ backend/StudentAndTeachersApp/settings.py
@@ -128,6 +128,19 @@ USE_TZ = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ORIGIN_WHITELIST = [
+    '*',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
