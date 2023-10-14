89e2e159f28d06b1e08da9f2b67ba3bde434e6d7
Bongomin Daniel <bongomindaniel@gmail.com>
2023-10-14 23:35:18 +0300

    change class to className

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -11,7 +11,7 @@ function Layout() {
             <NavBar />
             <div className="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
                 <SideBar />
-                <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
+                <div id="main-content" className="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
                     <main>
                         <TopPanel />
                     </main>
