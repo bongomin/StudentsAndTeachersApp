2a929123fc433c5ca562fb7bfa801df13998ead6
Bongomin Daniel <bongomindaniel@gmail.com>
2023-10-14 23:07:03 +0300

    backend folder

--- teachers/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Teacher
-from students.serializers import StudentSerializer
-
-
-class TeacherSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Teacher
-        fields = ['id', 'name', 'students', 'created_at']
