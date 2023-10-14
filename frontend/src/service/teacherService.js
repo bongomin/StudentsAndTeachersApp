a1a736ea23b03cc909900e5b7eb13152c0282ae1
Bongomin Daniel <bongomindaniel@gmail.com>
2023-10-14 23:17:01 +0300

    services

---
+++ frontend/src/service/teacherService.js
@@ -0,0 +1,37 @@
+import { AxiosInstance, baseURL } from "./axios"
+
+export const fetchTeachers = async () => {
+    try {
+        const response = await AxiosInstance.get(`${baseURL}teachers/`);
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+export const addTeachers = async (body) => {
+    try {
+        const response = await AxiosInstance.post(`${baseURL}teachers/`, body);
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+export const deleteTeachers = async (id) => {
+    try {
+        const response = await AxiosInstance.delete(`${baseURL}teachers/${id}`);
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+export const editTeachers = async (body) => {
+    try {
+        const response = await AxiosInstance.update(`${baseURL}teachers/${body.id}, body`);
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+};
