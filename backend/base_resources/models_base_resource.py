9a393909fc20658672ac3ab9417bdb4eda9d32dc
Bongomin Daniel <bongomindaniel@gmail.com>
2023-10-14 23:12:54 +0300

    base model

---
+++ backend/base_resources/models_base_resource.py
@@ -0,0 +1,15 @@
+import uuid
+from django.db import models
+
+
+class ModelsBaseResource(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['-created_at']
+        abstract = True
+
