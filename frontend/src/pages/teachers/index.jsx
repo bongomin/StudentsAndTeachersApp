9a49194e21a5bfeb6e91f00ea69ad49136e649a5
Bongomin Daniel <bongomindaniel@gmail.com>
2023-10-15 17:08:30 +0300

    use redux to retrieve data from store

--- frontend/src/pages/teachers/index.jsx
+++ frontend/src/pages/teachers/index.jsx
@@ -1,21 +1,29 @@
 import React, {
     useEffect,
-    useState
 } from 'react'
 import { fetchTeachers } from '../../service/teacherService';
 import TeachersTable from '../../components/TeachersTable';
+import { fetchStudents } from '../../service/studentService';
+import { loadStudents } from '../../store/studentSlice';
+import { useDispatch } from 'react-redux';
+import { loadTeachers } from '../../store/teacherSlice';
 
 const Teachers = () => {
-    const [teachers, setTeachers] = useState();
+    const dispatch = useDispatch();
+
     const fetch = async () => {
         const data = await fetchTeachers();
-        setTeachers(data.results);
+        const studentsData = await fetchStudents();
+        dispatch(loadStudents(studentsData.results))
+        dispatch(loadTeachers(data.results));
     }
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => { fetch() }, []);
 
     return (
-        <><TeachersTable teachers={teachers} />
+        <>
+            <TeachersTable />
         </>
     )
 };
