9b6cfe6c4409549adfaed80db4bfbd2ddbe2fa02
Bongomin Daniel <bongomindaniel@gmail.com>
2023-10-15 20:22:47 +0300

    use loader component

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,11 +1,13 @@
 import React, {
+    useEffect,
     useState
 } from 'react';
 import Modal from './Modal';
-import { deleteStudent, editStudent } from '../service/studentService';
+import { deleteStudent, editStudent, fetchStudents } from '../service/studentService';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeStudent, studentToUpdate, updatedStudent } from '../store/studentSlice';
+import { loadStudents, removeStudent, studentToUpdate, updatedStudent } from '../store/studentSlice';
 import { toast } from 'react-toastify';
+import Loader from './loader';
 
 const initialValues = {
     name: '',
@@ -17,6 +19,7 @@ function Table() {
     const students = useSelector(state => state.students.students);
     const dispatch = useDispatch();
     const [values, setValues] = useState(initialValues);
+    const [loading, setLoading] = useState(false)
 
     const [modal, setModal] = useState('');
     const [currentStudent, setCurrentStudent] = useState();
@@ -35,6 +38,17 @@ function Table() {
     const closeModal = () => {
         setModalVisible(false);
     };
+
+    const fetchAllStudents = async () => {
+        setLoading(true)
+        const response = await fetchStudents();
+        dispatch(loadStudents(response.results))
+        setLoading(false)
+
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => { fetchAllStudents() }, []);
 
     const deleteStudentFunction = (student) => {
         setModalVisible(true)
@@ -83,67 +97,71 @@ function Table() {
                         <th className="p-4">Actions</th>
                     </tr>
                 </thead>
-                {students && students.map((student, index) => (
-                    <tbody key={student.id} className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                        <tr className="hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <td className="w-4 p-4">{index + 1}</td>
-                            <td className="p-4 text-sm font-normal text-center text-gray-500 dark:text-gray-400">
-                                <div className="text-base font-semibold text-gray-900 dark:text-white">
-                                    {student.name}
-                                </div>
-                            </td>
-                            <td className="p-4 text-base font-medium text-gray-900 dark:text-white text-center">
-                                {student.surname}
-                            </td>
-                            <td className="p-4 text-base font-medium text-gray-900 dark:text-white text-center">
-                                <span className="inline-block bg-green-200 text-green-800 rounded-full px-2 py-1">
-                                    {formatDate(student.created_at)}
-                                </span>
-                            </td>
-                            <td className="p-4 space-x-2 whitespace-nowrap text-center">
-                                <button
-                                    type="button"
-                                    onClick={() => handleUpdateClick(student)}
-                                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-                                >
-                                    <svg
-                                        className="w-4 h-4 mr-2"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
+                <>
+                    {loading && <Loader />}
+                    {!loading && students?.length > 0 && students.map((student, index) => (
+                        <tbody key={student.id} className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                            <tr className="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                <td className="w-4 p-4">{index + 1}</td>
+                                <td className="p-4 text-sm font-normal text-center text-gray-500 dark:text-gray-400">
+                                    <div className="text-base font-semibold text-gray-900 dark:text-white">
+                                        {student.name}
+                                    </div>
+                                </td>
+                                <td className="p-4 text-base font-medium text-gray-900 dark:text-white text-center">
+                                    {student.surname}
+                                </td>
+                                <td className="p-4 text-base font-medium text-gray-900 dark:text-white text-center">
+                                    <span className="inline-block bg-green-200 text-green-800 rounded-full px-2 py-1">
+                                        {formatDate(student.created_at)}
+                                    </span>
+                                </td>
+                                <td className="p-4 space-x-2 whitespace-nowrap text-center">
+                                    <button
+                                        type="button"
+                                        onClick={() => handleUpdateClick(student)}
+                                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
                                     >
-                                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                                            clipRule="evenodd"
-                                        ></path>
-                                    </svg>
-                                    Update
-                                </button>
-                                <button
-                                    onClick={() => deleteStudentFunction(student)}
-                                    type="button"
-                                    data-drawer-placement="right"
-                                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
-                                >
-                                    <svg
-                                        className="w-4 h-4 mr-2"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
+                                        <svg
+                                            className="w-4 h-4 mr-2"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                                                clipRule="evenodd"
+                                            ></path>
+                                        </svg>
+                                        Update
+                                    </button>
+                                    <button
+                                        onClick={() => deleteStudentFunction(student)}
+                                        type="button"
+                                        data-drawer-placement="right"
+                                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
                                     >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                            clipRule="evenodd"
-                                        ></path>
-                                    </svg>
-                                    Delete
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>))}
+                                        <svg
+                                            className="w-4 h-4 mr-2"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                                clipRule="evenodd"
+                                            ></path>
+                                        </svg>
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    ))}
+                </>
             </table>
 
             {isModalVisible && modal === 'create' && (
