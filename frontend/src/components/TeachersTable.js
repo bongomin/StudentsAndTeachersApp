ed32c3085b151d08a9e470defc6406b096dab6f6
Bongomin Daniel <bongomindaniel@gmail.com>
2023-10-15 17:09:44 +0300

    list teachers in the TeachersTable

--- frontend/src/components/TeachersTable.js
+++ frontend/src/components/TeachersTable.js
@@ -1,24 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { filterStuddents } from '../utils/helpers';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteTeachers } from '../service/teacherService';
+import { removeTeacher, teacherToUpdate } from '../store/teacherSlice';
+import { useLocation } from 'react-router-dom';
 
-function TeachersTable(teachers) {
-    const sampleData = [
-        {
-            id: 1,
-            name: 'John',
-            students: [
-                { id: 1, name: 'Bongomin', surname: 'Kilama' },
-                { id: 2, name: 'Bongomin2', surname: 'Kilama2' },
-                { id: 3, name: 'Bongomin', surname: 'Kilama' },
-                { id: 4, name: 'Bongomin2', surname: 'Kilama2' }
-            ]
-        },
-        { id: 2, name: 'Sarah', students: [] },
-        { id: 3, name: 'Michael', students: [] }
-    ];
+function TeachersTable() {
+    const { teachers } = useSelector(state => state.teachers);
+    const students = useSelector(state => state.students.students);
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [currentTeacher, setCurrentTeacher] = useState({});
+    const [name, setName] = useState('');
+    const [selectedStudents, setSelectedStudents] = useState([]);
+
+    const { pathname } = useLocation();
+
+    const [modal, setModal] = useState('');
+
+    const dispatch = useDispatch();
+
+    const deleteTeacher = async (teacher) => {
+        const response = await deleteTeachers(teacher.id);
+        dispatch(removeTeacher(teacher.id))
+        console.log(response, "response data!!");
+    }
+
+    const updateTeacher = async (teacher) => {
+        setCurrentTeacher(teacher)
+        dispatch(teacherToUpdate(teacher))
+        setName(teacher.name)
+        setModalVisible(true);
+        setModal('create')
+    }
+
+    const handleClose = () => {
+        setModalVisible(false);
+        setModal('')
+    };
+
+    const handleStudentSelection = (studentId) => {
+        setSelectedStudents((prevSelectedStudents) => {
+            if (prevSelectedStudents.includes(studentId)) {
+                return prevSelectedStudents.filter((id) => id !== studentId);
+            } else {
+                return [...prevSelectedStudents, studentId];
+            }
+        });
+    };
+
+    useEffect(() => {
+        if(currentTeacher.students && currentTeacher.students.length > 0){
+            setSelectedStudents(currentTeacher?.students)
+        }
+    }, [currentTeacher])
 
     return (
         <div className="container mx-auto p-6">
-            {sampleData.map((teacher) => (
+            {teachers && teachers.map((teacher) => (
                 <div key={teacher.id} className="bg-white rounded-lg shadow-lg p-6 mb-6">
                     <table className="w-full">
                         <thead>
@@ -31,10 +69,14 @@ function TeachersTable(teachers) {
                             <tr className='text-center border'>
                                 <td className="p-2 font-semibold text-gray-900 text-left">{teacher.name}</td>
                                 <td className="p-4 space-x-2 text-right">
-                                    <button className="px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                                    <button
+                                        onClick={() => updateTeacher(teacher)}
+                                        className="px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                                         Update
                                     </button>
-                                    <button className="px-3 py-2 text-sm font-medium text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
+                                    <button
+                                        onClick={() => deleteTeacher(teacher)}
+                                        className="px-3 py-2 text-sm font-medium text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
                                         Delete
                                     </button>
                                 </td>
@@ -44,7 +86,7 @@ function TeachersTable(teachers) {
                     <div className="text-lg font-semibold mt-2 mb-2">Associated Students</div>
                     <div className="space-y-2">
                         {teacher.students.length > 0 ? (
-                            teacher.students.map((student) => (
+                            (filterStuddents(teacher.students, students)).map((student) => (
                                 <div
                                     key={student.id}
                                     className="bg-gray-100 p-4 rounded-lg"
@@ -66,7 +108,58 @@ function TeachersTable(teachers) {
                     </div>
                 </div>
             ))}
+            {isModalVisible && modal === 'create' && (
+                <div className="fixed inset-0 flex items-center justify-center z-50">
+                    <div className="modal-container p-12 bg-white rounded-lg shadow-lg w-96">
+                        <div className="text-xl font-semibold mb-4">Update Student</div>
+                        <div className="flex flex-col mb-4">
+                            <label htmlFor="name" className="mb-2">
+                                Name:
+                            </label>
+                            <input
+                                onChange={(e) => setName(e.target.value)}
+                                name='name' value={name} type="text" id="name" className="border p-2" placeholder="Enter name" />
+                            {
+                                students && (pathname.replace('/', '')) === 'teachers' &&
+                                <div className='p-2 border mb-'>
+                                    <p>Assign Student to Teacher:</p>
+                                    <hr className='p-2'></hr>
+                                    <ul className='m-2'>
+                                        {
+                                            students.map((student) => (
+                                                <li key={student.id}>
+                                                    <label>
+                                                        <input
+                                                            type="checkbox"
+                                                            onChange={() => handleStudentSelection(student.id)}
+                                                            checked={selectedStudents.includes(student.id)}
+                                                        />
+                                                        {`${student.name} ${student.surname}`}
+                                                    </label>
+                                                </li>
+                                            ))}
+                                    </ul>
+                                </div>
+                            }
+                        </div>
+                        <div className="flex justify-end">
+                            <button
+                                onClick={handleClose}
+                                className="bg-gray-400 text-white px-4 py-2 rounded-lg mr-2 hover:bg-gray-500"
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                className="bg-blue-700 text-white px-4 py-2 rounded-lg hover:bg-blue-800"
+                            >
+                                Update
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
+
     );
 }
 
